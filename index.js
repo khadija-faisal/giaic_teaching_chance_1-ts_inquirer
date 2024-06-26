a6c2581dfb5a72b52f3b656702fce745d554c6a0
khadija-faisal <khadijamughal1119@gmail.com>
2024-06-26 06:35:20 -0700

    add code

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-"use strict";
-console.log('You are the best developer :)');
+import inquirer from "inquirer";
+const signIn = await inquirer.prompt([
+    {
+        type: "input",
+        name: "username",
+        message: "enter your username",
+    },
+    {
+        type: "number",
+        name: "password",
+        message: "enter your password",
+    },
+    {
+        type: "string",
+        name: "email",
+        message: "enter your email",
+    },
+    {
+        type: "list",
+        name: "profession",
+        message: "select your profession",
+        choices: ["software-Engineer", "student", "Developer", "UI/UX Designer"],
+    },
+]);
+console.log(signIn);
--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-console.log('You are the best developer :)');
+//// Import the inquirer package,
+import inquirer from "inquirer";
+
+//build a prompt to collect user information for signing in.
+
+const signIn = await inquirer.prompt([
+  // Prompt the user to enter their username.
+  {
+    type: "input",
+    name: "username",
+    message: "enter your username",
+  },
+  // Prompt the user to enter their password.
+  {
+    type: "number",
+    name: "password",
+    message: "enter your password",
+  },
+  // Prompt the user to enter their email
+  {
+    type: "string",
+    name: "email",
+    message: "enter your email",
+  },
+  // Prompt the user to select their profession from a list of options.
+  {
+    type: "list",
+    name: "profession",
+    message: "select your profession",
+    choices: ["software-Engineer", "student", "Developer", "UI/UX Designer"],
+  },
+]);
+
+//simply Log the user's input to the console
+console.log(signIn);
